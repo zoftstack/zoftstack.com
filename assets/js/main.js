f144b30f36c00188b96d524ecce611cf6a2a6f81
mafei <mafei.dev@gmail.com>
2023-03-24 04:32:47 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,231 +1,275 @@
-/**
-* Template Name: KnightOne - v4.10.0
-* Template URL: https://bootstrapmade.com/knight-simple-one-page-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
-  "use strict";
+/*--------------------------
+    Project Name: Mintech
+    Version: 1.0
+    Author: 7oorof
+    Devloped by: Ahmed Abdallah (a.abdallah999@gmail.com)
+    Relase Date: October 2020
+---------------------------*/
+/*---------------------------
+      Table of Contents
+    --------------------
+    01- Pre Loading
+    02- Mobile Menu
+    03- Sticky Navbar
+    04- Scroll Top Button
+    05- Close Topbar
+    06- Set Background-img to section 
+    07- Add active class to accordions
+    08- Contact Form validation
+    09- Slick Carousel
+    10- Popup Video
+    11- CounterUp
+    12- NiceSelect Plugin
+    13- portfolio Filtering and Sorting
+     
+ ----------------------------*/
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+$(function () {
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
+    "use strict";
 
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    // Global variables
+    var $win = $(window);
 
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
+    /*==========  Pre Loading   ==========*/
+    $win.on('load', function () {
+        $(".preloader").fadeOut(5000);
+        $(".preloader").remove();
     });
-  }
 
-  /**
-   * Initiate glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
+    /*==========   Mobile Menu   ==========*/
+    var $navToggler = $('.navbar-toggler');
+    $navToggler.on('click', function () {
+        $(this).toggleClass('actived');
+    })
+    $navToggler.on('click', function () {
+        $('.navbar-collapse').toggleClass('menu-opened');
+    })
 
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
+    /*==========   Sticky Navbar   ==========*/
+    $win.on('scroll', function () {
+        if ($win.width() >= 992) {
+            var $stickyNavbar = $('.sticky-navbar'),
+                $secondaryNavbar = $('.secondary-nav');
+            if ($win.scrollTop() > 50) {
+                $stickyNavbar.addClass('is-sticky');
+            } else {
+                $stickyNavbar.removeClass('is-sticky');
+            }
+            if ($secondaryNavbar.length) {
+                if ($win.scrollTop() > $secondaryNavbar.offset().top - 100) {
+                    $secondaryNavbar.addClass('secondary-nav-sticky');
+                } else {
+                    $secondaryNavbar.removeClass('secondary-nav-sticky');
+                }
+            }
+        }
+    });
+    // Scroll To Section when Clicking on The Link
+    $('.secondary-nav-internal-navigation .nav__link').on("click", function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('scroll')).offset().top - 140
+        }, 1000);
+    });
+
+    // Add  active class when The Scroll Reaching the Section
+    $(window).on("scroll", function () {
+        $('section').each(function () {
+            if ($(window).scrollTop() > $(this).offset().top - 141) {
+                var sectionID = $(this).attr('id');
+                $('.secondary-nav-internal-navigation .nav__link').removeClass('active');
+                $('.secondary-nav-internal-navigation .nav__link[data-scroll="' + sectionID + '"]').addClass('active');
+            }
         });
-        this.classList.add('filter-active');
+    });
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
+    /*==========   Scroll Top Button   ==========*/
+    var $scrollTopBtn = $('#scrollTopBtn');
+    // Show Scroll Top Button
+    $win.on('scroll', function () {
+        if ($(this).scrollTop() > 700) {
+            $scrollTopBtn.addClass('actived');
+        } else {
+            $scrollTopBtn.removeClass('actived');
+        }
+    });
+    // Animate Body after Clicking on Scroll Top Button
+    $scrollTopBtn.on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    /*==========   Close Topbar   ==========*/
+    $('.topbar__close').on("click", function (e) {
+        e.preventDefault();
+        $(this).closest('.topbar').fadeOut()
+    });
+
+    /*==========   Set Background-img to section   ==========*/
+    $('.bg-img').each(function () {
+        var imgSrc = $(this).children('img').attr('src');
+        $(this).parent().css({
+            'background-image': 'url(' + imgSrc + ')',
+            'background-size': 'cover',
+            'background-position': 'center',
         });
+        $(this).parent().addClass('bg-img');
+        if ($(this).hasClass('background-size-auto')) {
+            $(this).parent().addClass('background-size-auto');
+        }
+        $(this).remove();
+    });
 
-      }, true);
+    /*==========   Add active class to accordions   ==========*/
+    $('.accordion-item__header').on('click', function () {
+        $(this).parent('.accordion-item').toggleClass('opened');
+        $(this).parent('.accordion-item').siblings().removeClass('opened');
+    })
+    $('.accordion-item__title').on('click', function (e) {
+        e.preventDefault()
+    });
+
+    /*==========  Open and Close Popup   ==========*/
+    // open Popup
+    function openPopup(popupTriggerBtn, popup, addedClass, removedClass) {
+        $(popupTriggerBtn).on('click', function (e) {
+            e.preventDefault();
+            $(popup).toggleClass(addedClass, removedClass).removeClass(removedClass);
+        });
     }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+    // Close Popup
+    function closePopup(closeBtn, popup, addedClass, removedClass) {
+        $(closeBtn).on('click', function () {
+            $(popup).removeClass(addedClass).addClass(removedClass);
+        });
     }
-  });
+    // close popup when clicking on an other place on the Document
+    function closePopupFromOutside(popup, stopPropogationElement, popupTriggerBtn, removedClass, addedClass) {
+        $(document).on('mouseup', function (e) {
+            if (!$(stopPropogationElement).is(e.target) && !$(popupTriggerBtn).is(e.target) && $(stopPropogationElement).has(e.target).length === 0 && $(popup).has(e.target).length === 0) {
+                $(popup).removeClass(removedClass).addClass(addedClass);
+            }
+        });
+    }
+    openPopup('.action__btn-search', '.search-popup', 'active', 'inActive') // Open sidenav popup
+    closePopup('.search-popup__close', '.search-popup', 'active', 'inActive') // Close sidenav popup
+    openPopup('.action__btn-burgerMenu', '.burger-menu', 'active', 'inActive') // Open sidenav popup
+    closePopup('.burger-menu__close', '.burger-menu', 'active', 'inActive') // Close sidenav popup
+    openPopup('.action__btn-cart', '.cart-popup', 'active', 'inActive') // Open Search popup
+    closePopupFromOutside('.burger-menu', '.burger-menu__content', '.action__btn-burgerMenu', 'active', 'inActive');  // close popup when clicking on an other place on the Document
+    openPopup('.action__btn-menuPopup', '.menu-popup', 'active', 'inActive') // Open menu-popup
+    closePopup('.menu-popup__close', '.menu-popup', 'active', 'inActive') // Close menu-popup
 
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
+    openPopup('.open-login-popup', '#login-popup', 'active', 'inActive') // Open sidenav popup
+    closePopupFromOutside('#login-popup', '.login-popup-wrapper', '.open-login-popup', 'active', 'inActive');  // close popup when clicking on an other place on the Document
 
-})()
+    openPopup('.open-register-popup', '#register-popup', 'active', 'inActive') // Open sidenav popup
+    closePopupFromOutside('#register-popup', '.login-popup-wrapper', '.open-register-popup', 'active', 'inActive');  // close popup when clicking on an other place on the Document
+    // Open Login Popup
+    $('#go-login').on('click', function () {
+        $('#register-popup').removeClass('active').addClass('inActive');
+        $('#login-popup').removeClass('inActive').addClass('active');
+    });
+    // Open Register Popup
+    $('#go-register').on('click', function () {
+        $('#login-popup').removeClass('active').addClass('inActive');
+        $('#register-popup').removeClass('inActive').addClass('active');
+    });
+
+    /*==========  Contact Form validation  ==========*/
+    var contactForm = $("#contactForm"),
+        contactResult = $('.contact-result');
+    contactForm.validate({
+        debug: false,
+        submitHandler: function (contactForm) {
+            $(contactResult, contactForm).html('Please Wait...');
+            $.ajax({
+                type: "POST",
+                url: "assets/php/contact.php",
+                data: $(contactForm).serialize(),
+                timeout: 20000,
+                success: function (msg) {
+                    $(contactResult, contactForm).html('<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>').delay(3000).fadeOut(2000);
+                },
+                error: $('.thanks').show()
+            });
+            return false;
+        }
+    });
+
+    /*==========   Slick Carousel ==========*/
+    $('.slick-carousel').slick();
+
+    $('.slider-with-navs').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        dots: false,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-with-navs',
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+        centerPadding: '10px'
+    });
+
+    /*==========  Popup Video  ==========*/
+    $('.popup-video').magnificPopup({
+        mainClass: 'mfp-fade',
+        removalDelay: 0,
+        preloader: false,
+        fixedContentPos: false,
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: '//www.youtube.com/embed/%id%?autoplay=1'
+                }
+            },
+            srcAction: 'iframe_src',
+        }
+    });
+    $('.popup-gallery-item').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+    /*==========   counterUp  ==========*/
+    $(".counter").counterUp({
+        delay: 10,
+        time: 4000
+    });
+
+    /*==========  NiceSelect Plugin  ==========*/
+    $('select').niceSelect();
+
+    /*==========   portfolio Filtering and Sorting  ==========*/
+    $("#filtered-items-wrap").mixItUp();
+    $(".portfolio-filter li a").on("click", function (e) {
+        e.preventDefault();
+    });
+});
